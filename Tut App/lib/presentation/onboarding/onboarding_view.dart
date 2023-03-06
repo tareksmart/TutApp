3a3fb2b3a21c0dd91785f8f25f37352177fc222a
tareksmart <tareksmart2015@gmail.com>
2023-03-06 23:41:14 +0200

    from home pc

---
+++ Tut App/lib/presentation/onboarding/onboarding_view.dart
@@ -0,0 +1,211 @@
+import 'package:advanced_flutter_arabic/presentation/resources/assets_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/constants_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/routes_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/strings_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/values_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnBoardingView extends StatefulWidget {
+  const OnBoardingView({Key? key}) : super(key: key);
+
+  @override
+  _OnBoardingViewState createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s0_64,
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: ColorManager.darkPrimary,
+            statusBarBrightness: Brightness.dark),
+      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            return OnBoardingPage(_list[index]);
+          }),
+      bottomSheet: Container(
+        color: ColorManager.white,
+        //height: AppSize.s100,//كنا حاطين ارتفاع الكونينر كان بيعمل مساحة بيضاء تحت لغيناها وعمانا حاجة فى ال column
+        child: Column(
+          mainAxisSize: MainAxisSize
+              .min, //هنا تغلبنا على المساحة البيضاء تحت bottom sheet
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.loginRoute);
+                },
+                child: Text(
+                  AppStrings.skip,
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ),
+
+            // widgets indicator and arrows
+            _getBottomSheetWidget(size)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getBottomSheetWidget(Size size) {
+    return Container(
+      height: size.height * AppSize.s0_64,
+      color: ColorManager.lightPrimary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // left arrow
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                width: AppSize.s20,
+                height: AppSize.s18,
+                child: SvgPicture.asset(ImageAssets.leftArrowIc),
+              ),
+              onTap: () => _pageController.animateToPage(_getPreviospageIndex(),
+                  duration: const Duration(
+                      microseconds: AppConstants.slderAnimationTime),
+                  curve: Curves.bounceIn),
+            ),
+          )
+
+          // circle indicator
+
+          // right arrow
+          ,
+          Row(
+            children: [
+              for (int i = 0; i < _list.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(AppPadding.p8),
+                  child: _getProperCircle(i),
+                )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                width: AppSize.s20,
+                height: AppSize.s18,
+                child: SvgPicture.asset(ImageAssets.rightArrowIc),
+              ),
+              onTap: () => _pageController.animateToPage(_getNextPageIndex(),
+                  duration: const Duration(
+                      microseconds: AppConstants.slderAnimationTime),
+                  curve: Curves.bounceIn),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  int _getPreviospageIndex() {
+    int prevIndex = --_currentIndex;
+    if (prevIndex == -1) {
+      prevIndex = _list.length-1;
+    }
+    print("prevIndex=====");
+    print(prevIndex);
+    return prevIndex; //
+  }
+
+  int _getNextPageIndex() {
+    int nextIndex = ++_currentIndex;
+    if (nextIndex == _list.length) {
+      nextIndex = 0;
+    }
+    print("nextIndex=====");
+    print(nextIndex);
+    return nextIndex; //_getNextPageIndex
+  }
+
+  Widget _getProperCircle(int index) {
+    if (index == _currentIndex) {
+      return SvgPicture.asset(ImageAssets.hollowCircleIc);
+    } else {
+      return SvgPicture.asset(ImageAssets.solidCircleIc);
+    }
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  final SliderObject _sliderObject;
+
+  const OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(height: AppSize.s40),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        const SizedBox(height: AppSize.s60),
+        SvgPicture.asset(_sliderObject.image)
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
+}
